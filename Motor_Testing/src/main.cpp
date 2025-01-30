6c928e817357b780f596e23159c33ad13c521e36
BlackDemon1000 <gw1_10@gmx.de>
2025-01-30 21:12:49 +0100

    another test code

--- Motor_Testing/src/main.cpp
+++ Motor_Testing/src/main.cpp
@@ -1,3 +1,19 @@
+/*******************************************************************************
+* Copyright 2016 ROBOTIS CO., LTD.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
 #include <DynamixelShield.h>
 
 #if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
@@ -23,76 +39,37 @@ void setup() {
   
   // For Uno, Nano, Mini, and Mega, use UART port of DYNAMIXEL Shield to debug.
   DEBUG_SERIAL.begin(115200);
-  
+
   // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
   dxl.begin(57600);
   // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
   dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
   // Get DYNAMIXEL information
   dxl.ping(DXL_ID);
+
+  // Turn off torque when configuring items in EEPROM area
+  dxl.torqueOff(DXL_ID);
+  dxl.setOperatingMode(DXL_ID, OP_POSITION);
+  dxl.torqueOn(DXL_ID);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  // Position Control Mode in protocol2.0, Joint Mode in protocol1.0
-  // Turn off torque when configuring items in EEPROM area
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 512)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
-  }
+  // Please refer to e-Manual(http://emanual.robotis.com/docs/en/parts/interface/dynamixel_shield/) for available range of value. 
+  // Set Goal Position in RAW value
+  dxl.setGoalPosition(DXL_ID, 512);
+  delay(1000);
+  // Print present position in raw value
+  DEBUG_SERIAL.print("Present Position(raw) : ");
+  DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID));
+  delay(1000);
 
-  // Velocity Contorl Mode in protocol2.0, Speed Mode in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_VELOCITY);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalVelocity(DXL_ID, 128)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Velocity : ");
-    DEBUG_SERIAL.println(dxl.getPresentVelocity(DXL_ID)); DEBUG_SERIAL.println();
-  }
-
-  // Extended Position Contorl Mode in protocol2.0, Multi-turn Mode(only MX series) in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_EXTENDED_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 4096)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Extended Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
-  }
-
-  // PWM Contorl Mode in protocol2.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_PWM);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPWM(DXL_ID, 200)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present PWM : ");
-    DEBUG_SERIAL.println(dxl.getPresentPWM(DXL_ID)); DEBUG_SERIAL.println();
-  }
-  
-  // Current Contorl Mode in protocol2.0, Torque Mode(only MX64,MX106) in protocol1.0
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_CURRENT);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalCurrent(DXL_ID, 256)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Current : ");
-    DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID)); DEBUG_SERIAL.println();
-  }
-  
-  // Current Based Postion Contorl Mode in protocol2.0 (except MX28, XL430)
-  dxl.torqueOff(DXL_ID);
-  dxl.setOperatingMode(DXL_ID, OP_CURRENT_BASED_POSITION);
-  dxl.torqueOn(DXL_ID);
-  if(dxl.setGoalPosition(DXL_ID, 8192)){
-    delay(1000);
-    DEBUG_SERIAL.print("Present Current Based Position : ");
-    DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID)); DEBUG_SERIAL.println();
-  }
+  // Set Goal Position in DEGREE value
+  dxl.setGoalPosition(DXL_ID, 5.7, UNIT_DEGREE);
+  delay(1000);
+  // Print present position in degree value
+  DEBUG_SERIAL.print("Present Position(degree) : ");
+  DEBUG_SERIAL.println(dxl.getPresentPosition(DXL_ID, UNIT_DEGREE));
+  delay(1000);
 }
