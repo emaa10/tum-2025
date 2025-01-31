9f784f5ff9c756e0a196feefaf518cf516b3ee92
BlackDemon1000 <gw1_10@gmx.de>
2025-01-31 07:26:52 +0100

    mehr wumms

--- Motor_Testing/src/main.cpp
+++ Motor_Testing/src/main.cpp
@@ -66,7 +66,10 @@ int Motorrechts_Geschw = 100;  //Variable für lesen aktuelle RPM Motor rechts
 
 void loop() {
     //bloß zum testen, hat noch nen schlaffen
-    dxl.setGoalVelocity(Motor_links, -80, UNIT_PERCENT);
-    dxl.setGoalVelocity(Motor_rechts, 20, UNIT_PERCENT);
+    dxl.setGoalVelocity(Motor_links, -1, UNIT_PERCENT);
+    dxl.setGoalVelocity(Motor_rechts, 99, UNIT_PERCENT);
+    Motorlinks_Geschw = dxl.getPresentVelocity(2);  //Übergebe derzeitige Drehzahl als Raw Wert dxl.getPresentVelocity(Motor_ID , Einheit) => Einheit: keine Eingabe=RAW | UNIT_RPM=Drehzahl pro Min | UNIT_PERCENT =%
+    Motorrechts_Geschw = dxl.getPresentVelocity(1);
+
     delay(550);
 }
